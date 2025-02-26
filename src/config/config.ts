f9c5127a4c453e5a40158d8fdbb6c9f194914742
rykova_ed <katrykova2002@yandex.ru>
2025-02-25 21:26:33 -0800

    .

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,6 @@ export default () => ({
         secret: process.env.JWT_SECRET
     },
     database: {
-        connectionString: process.env.MONGO_URL || 'mongodb+srv://katrykova1:Fizika852@cluster0.w4nct.mongodb.net/'
+        connectionString: process.env.MONGO_URL || 'mongodb+srv://katrykova1:Fizika852@cluster0.w4nct.mongodb.net?retryWrites=true&w=majority'
     }
 })
